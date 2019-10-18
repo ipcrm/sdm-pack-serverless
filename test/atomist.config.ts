5c37ff6f083e0662ccba2ce13be833d3cbda1567
ipcrm <mcadorette@gmail.com>
2019-10-18 13:06:33 -0400

    Update test SDM

--- test/atomist.config.ts
+++ test/atomist.config.ts
@@ -1,50 +1,23 @@
 import {
-    Configuration,
-} from "@atomist/automation-client";
-import {
-    not,
-    SoftwareDeliveryMachine,
-    SoftwareDeliveryMachineConfiguration, whenPushSatisfies,
-} from "@atomist/sdm";
-import {
-    configureSdm,
-    createSoftwareDeliveryMachine,
+    configure,
 } from "@atomist/sdm-core";
 import {ServerlessDeploy} from "../lib/goal/deploy";
 import {serverlessSupport} from "../lib/serverless";
 import {IsServerlessDeployable} from "../lib/support/pushTest";
 
-export function machineMaker(config: SoftwareDeliveryMachineConfiguration): SoftwareDeliveryMachine {
-
-    const sdm = createSoftwareDeliveryMachine(
-        {
-            name: `${configuration.name}-test`,
-            configuration: config,
-        },
-    );
-
-    const dev = new ServerlessDeploy()
-        .with({
-            deployArgs: { stage: "dev" },
-        });
-
+export const configuration = configure(async sdm => {
     sdm.addExtensionPacks(
         serverlessSupport(),
     );
-
-    sdm.withPushRules(
-        whenPushSatisfies(IsServerlessDeployable)
-            .setGoals(dev),
-    );
-
-    return sdm;
-}
-
-export const configuration: Configuration = {
-    postProcessors: [
-        configureSdm(machineMaker),
-            async c => {
-                return c;
-            },
-    ],
-};
+    return {
+        serverless: {
+            test: IsServerlessDeployable,
+            goals: [
+                new ServerlessDeploy()
+                    .with({
+                        deployArgs: { stage: "dev" },
+                    }),
+            ],
+        },
+    };
+});
